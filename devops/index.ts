ff2771b1c0bc73b12077cbed4d87f0085f88414c
Yash Narwal <ynarwal10@gmail.com>
2020-03-02 23:00:16 +1100

    expose ipAddress of ingress controller on index

--- devops/index.ts
+++ devops/index.ts
@@ -1,6 +1,7 @@
 import * as cluster from './cluster'
-import './cert-maanger';
-import './ingress';
+import './cert-manager';
+import * as ingress from './ingress';
 import './resume';
 
 export const kubeconfig = cluster.k8sConfig;
+export const resumeIngressControllerIp = ingress.ipAddress.address;
